2c5632955f26572d98e07b918648c0d37e2c71ee
Azis-Hakim <90516483+Azis-Hakim@users.noreply.github.com>
2022-08-08 16:43:39 +0700

    Create bash.bashrc.sh

---
+++ bash.bashrc.sh
@@ -0,0 +1,45 @@
+clear
+
+PS1='\e[1;92mAziz_official-root#   '
+
+echo -e '\e[92m                       -:/++oosssyyyyyyso+/:---
+                 :+oydmmNNNNNNNNNNNMMMMMMMMMNdyo/--
+               /shddmmmmmNNNNNNNNNNMMMMMMMMMMMMMMNh+
+              osyhddmmmmmNNNNNNNNNNMMMMMMMMMMMMMMMMMm/
+             ssyhdddmmmNNNNNNNNNNNMMMMMMMMMMMMMMMMMMNh
+            :sso+/:::://+ohmNNNNNNNNNNNNmddhysyhmNMMMNh
+            /s/.`.....``-/+o+hmNNNNNmdsoo+:::://::sNNMN
+            ++..:++++::-..`.ssodNNmo:..:/oydNMMMNms/NNNs
+            o++yddmmmmmho::/yhmmNNNms+osdNNNMMMMMMMmmNNd
+            ssyhhdhssyhhdds:`-dNNMNs/ymNNmddmdmNNNNNNNNm
+            ssyhh//::---:/o+:-sNMMMNNdhyo----::shNNNNNNN
+            ys+o:++/     /+ososNMMMMNdy/-.`````./mdmmmMM
+            ysydmdhhhddmmmNNdssNMMMMMMMNNNNNNNNNNNNNNNNN
+            sdmmNNNNNNNNNNNmhsyNMMMMMMMMMMMMMMMMMMMMMNNN
+            yhmmmmmmmNNNNNNdhyhNMMMMMMMMMMMMMMMMMMMMMMNN
+            yyhhdddmmmmmmdddhsdNMMMMMMMMNNMMMNNNNNNNMNNN
+            ysoossssso++oohdyshNNMMMMMMMNhyhdmNNNNNNNNNN
+            +s/+/-++oydmhso-:o+oyyhdy+yNMMMmdysssyhdmNNm
+            -o:/s/.smmmmdhys+``.--.odmNMMMMMMMm/.shmNNNy
+             ++o+s+`/hdhyyyo`  :h+``.omNMMMNmo`-hdmNNNN/
+             -/+s+os-.``````./hmNNh:.```..```:ymdNNNmNh:
+              //+s+/oyhddhyossooosssssdNNNNNNNdmNNmmmN/
+               +//os++++osssyhdmmNNNNNNmmdmmddNMNmmmNo
+                /+/+hs+osso++/-``../osssmNNmmNNmmmNm+
+                 .///yhoshdmmNm- `/NNNNNNNmNMNmmmms
+                   .::+dhshmNNm. `-MMMMNNNNNmmNdo
+                     `-/ydydmNd` ``NMMMMNNNmmy/
+                       `-+oshmd`  :NNNNNNmh+
+                         `-/oyd: `dNNNNdo-
+                           `.-//-yhys+-\e[0m'
+
+printf "\e[1;92m   ____  _____  ____  _____       ___   _____  _____  ____     __  ____>
+  /    T|     Tl    j|     T     /   \ |     ||     |l    j   /  ]l    j /    T| T
+ Y  o  |l__/  | |  T l__/  |    Y     Y|   __j|   __j |  T   /  /  |  T Y  o  || |
+ |     ||   __j |  | |   __j    |  O  ||  l_  |  l_   |  |  /  /   |  | |     || l___  >
+ |  _  ||  /  | |  | |  /  |    |     ||   _] |   _]  |  | /   \_  |  | |  _  ||     T
+ |  |  ||     | j  l |     |    l     !|  T   |  T    j  l \     | j  l |  |  ||     |
+ l__j__jl_____j|____jl_____j     \___/ l__j   l__j   |____j \____j|____jl__j__jl_____j\>
+
+echo "      "
+echo "    "
